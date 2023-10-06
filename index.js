cfde402d2f2ba8bed3b1be486480d4e339b2a8dc
MarianoSagua <marianosagua4343@gmail.com>
2023-10-06 20:30:10 -0300

    Commit 7

--- index.js
+++ index.js
@@ -14,8 +14,22 @@ app.use(express.json());
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/events", require("./routes/events.routes"));
 
-app.get("*", (req, res) => {
-  res.send(__dirname + "/public/index.html");
+// app.get("*", (req, res) => {
+//   res.send(__dirname + "/public/index.html");
+// });
+
+app.get("/", (req, res) => {
+  const htmlResponse = `
+    <html>
+      <head>
+        <title>NodeJs y Express</title>
+      </head>
+      <body>
+        <h1>Calendar Backend</h1>
+      </body>
+    </html>
+  `;
+  res.send(htmlResponse);
 });
 
 app.listen(port, () => {
